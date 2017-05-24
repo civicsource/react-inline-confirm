7964137e9fb3ac414b118de4d6d48850b067d125
Andrés Barreto <andres.barreto@archoninfosys.com>
2017-05-24 16:31:54 -0500

    Fix 'Accessing PropTypes via the main React package is deprecated' warning (#4)
    * Use prop-types package
    
    * Bump version

--- index.jsx
+++ index.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes as t } from "react";
+import React, { Component } from "react";
+import { PropTypes as t } from "prop-types";
 
 export default class InlineConfirmButton extends Component {
 	constructor(props) {
